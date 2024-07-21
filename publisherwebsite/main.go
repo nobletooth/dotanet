7738628e924468d425e8819d36c127f39195297e
Amin-mhr <amin.mhr2001@gmail.com>
2024-07-21 15:18:57 +0330

    add html template and teplate for 3 api.

--- publisherwebsite/main.go
+++ publisherwebsite/main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	pgConnection()
+	//pgConnection()
 	router := gin.Default()
+	router.LoadHTMLGlob("./publisherwebsite/html/*")
 
-	router.GET("/")
-	router.POST("/")
+	router.GET("/torob", torobHandler())
+	router.GET("/samsung", samsungHandler())
+	router.GET("/digiland", digilandHandler())
 
 	router.Run(":6060")
+}
+
+func torobHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(http.StatusOK, "varzesh3.html", gin.H{})
+	}
+}
+
+func samsungHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(http.StatusOK, "varzesh3.html", gin.H{})
+	}
+}
+
+func digilandHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.HTML(http.StatusOK, "varzesh3.html", gin.H{})
+	}
 }
