90e3a2fc12a9df0baea402a1d05136040fbc92d0
Dena <denasalami78@gmail.com>
2024-07-22 00:35:21 +0330

    add UpdateAd function, DeleteAd and form to Ad.go

--- panel/advertiser/ad.go
+++ panel/advertiser/ad.go
@@ -1,27 +1,17 @@
 package advertiser
 
 import (
-	"dotanet/db"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
-
-type AdInfo struct {
-	Id           uint    `json:"id"`
-	Title        string  `json:"title"`
-	Image        string  `json:"image"`
-	Price        float64 `json:"price"`
-	Status       bool    `json:"status"`
-	Url          string  `json:"url"`
-	AdvertiserId uint    `gorm:"foreignKey:advertiserid"`
-}
 
 type Ad struct {
 	Id           uint    `gorm:"column:id;primary_key"`
 	Title        string  `gorm:"column:title"`
-	Image        string  `gorm:"column:image"`
+	Image        string  `:image"`
 	Price        float64 `gorm:"column:price"`
 	Status       bool    `gorm:"column:status"`
 	Clicks       int     `gorm:"column:clicks"`
@@ -30,45 +20,74 @@ type Ad struct {
 	AdvertiserId uint    `gorm:"foreignKey:advertiserid"`
 }
 
-func (ad *AdInfo) CreateAd(db *db.Database) gin.HandlerFunc {
-
+func CreateAd(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if ad.Title == "" || ad.Image == "" || ad.Price == 0 || ad.Url == "" || ad.AdvertiserId == 0 {
+		var form Ad
+		if err := c.Bind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if form.Title == "" || form.Image == "" || form.Price == 0 || form.Url == "" || form.AdvertiserId == 0 {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Missing required fields"})
 			return
 		}
-		ad.Status = true
-		entity := MapEntity(*ad)
-		if err := db.DB.Create(&entity).Error; err != nil {
+
+		form.Status = true
+		if err := db.Create(&form).Error; err != nil {
 			log.Printf("Creating ad failed: %v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Creating ad failed"})
 			return
 		}
-		c.JSON(http.StatusCreated, gin.H{"message": "Ad created successfully", "ad": entity})
-	}
-
-}
-
-func MapEntity(ad AdInfo) Ad {
-	return Ad{
-		Id:           ad.Id,
-		Title:        ad.Title,
-		Image:        ad.Image,
-		Price:        ad.Price,
-		Status:       ad.Status,
-		Url:          ad.Url,
-		AdvertiserId: ad.AdvertiserId,
-		Clicks:       0,
-		Impressions:  0,
+		c.JSON(http.StatusCreated, gin.H{"message": "Ad created successfully", "ad": form})
 	}
 }
 
-func CTR(entity Ad) float64 {
-	if entity.Impressions == 0 {
-		return 0
+func DeleteAd(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		adID := c.Param("id")
+		var ad Ad
+		if err := db.First(&ad, adID).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				c.JSON(http.StatusNotFound, gin.H{"error": "Ad not found"})
+			} else {
+				log.Printf("Error finding ad: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error finding ad"})
+			}
+			return
+		}
+		if err := db.Delete(&ad).Error; err != nil {
+			log.Printf("Deleting ad failed: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Deleting ad failed"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Ad deleted successfully"})
 	}
-	return float64(entity.Clicks) / float64(entity.Impressions)
 }
-func CostCalculator(entity Ad) float64 {
-	return float64(entity.Clicks) * entity.Price
+func UpdateAd(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		adID := c.Param("id")
+		var ad Ad
+		if err := db.First(&ad, adID).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				c.JSON(http.StatusNotFound, gin.H{"error": "Ad not found"})
+			} else {
+				log.Printf("Error finding ad: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error finding ad"})
+			}
+			return
+		}
+
+		var updatedForm Ad
+		if err := c.Bind(&updatedForm); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if err := db.Model(&ad).Updates(updatedForm).Error; err != nil {
+			log.Printf("Updating ad failed: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Updating ad failed"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Ad updated successfully", "ad": updatedForm})
+	}
 }
