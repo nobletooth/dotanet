591ad633ae3899577724d8d87739cdada1ad027f
dena <38652032+dena99@users.noreply.github.com>
2024-07-21 17:46:21 +0330

    Update advertiser

--- panel/advertiser/advertiser.go
+++ panel/advertiser/advertiser.go
@@ -3,33 +3,59 @@ package advertiser
 import "example.com/dotanet/db"
 
 type AdvertiserRequestDto struct {
-	ID      int `json:"id"`
-	Creadit int `json:"amount"`
+ Creadit int    json:"amount"
+ Name    string json:"name"
 }
 
 type AdvertiserEntity struct {
-	ID      int  `gorm:"column:id;primary_key"`
-	Creadit bool `gorm:"column:creadit"`
+ ID     uint   gorm:"primaryKey;autoIncrement" // Unique identifier, auto-incrementing
+ Name   string gorm:"unique;not null"          // Unique name, optional but ensures that the name is unique
+ Credit int    gorm:"column:credit"            // Fixed typo for the column name
 }
 
 type Advertiserservice interface {
-	// AddTask(dto.TodoRequestBody) (entitys.TodoLists, error)
-	// ListTasks() ([]entitys.TodoLists, error)
-	// UpdateTask(int, dto.TodoRequestBody) error
-	// RemoveTask(int) error
-	// GetTask(int) (entitys.TodoLists, error)
-	CreateAdvertiserEntity(AdvertiserEntity) AdvertiserEntity
+ GetCreaditOfAdvertiser(adId int) (int, error)
+ CreateAdvertiserEntity(AdvertiserRequestDto)
+ ListAllAdvertiserEntity() []AdvertiserEntity
+ FindAdvertiserByName(name string) (AdvertiserEntity, error)
 }
 
 type AdvertiService struct {
-	db *db.Database
+ db *db.Database
 }
 
 func NewAdvertiserService(db *db.Database) AdvertiService {
-	AdvertiSerservice := AdvertiService{db: db}
-	return AdvertiSerservice
+ return AdvertiService{db: db}
 }
 
-func (p *AdvertiService) CreateAdvertiserEntity(entity AdvertiserEntity) {
-	p.db.DB.Create(&entity)
+func (p *AdvertiService) CreateAdvertiserEntity(dto AdvertiserRequestDto) {
+ entity := AdvertiserEntity{
+  Credit: dto.Creadit,
+  Name:    dto.Name,
+ }
+ p.db.DB.Save(&entity)
+}
+
+func (p *AdvertiService) ListAllAdvertiserEntity() []AdvertiserEntity {
+ var advertisers []AdvertiserEntity
+ p.db.DB.Find(&advertisers)
+ return advertisers
+}
+
+func (p *AdvertiService) GetCreaditOfAdvertiser(adId int) (int, error) {
+ var entity AdvertiserEntity
+ result := p.db.DB.First(&entity, adId)
+ if result.Error != nil {
+  return 0, result.Error
+ }
+ return entity.Creadit, nil
+}
+
+func (p *AdvertiService) FindAdvertiserByName(name string) (AdvertiserEntity, error) {
+ var entity AdvertiserEntity
+ result := p.db.DB.Where("name = ?", name).First(&entity)
+ if result.Error != nil {
+  return AdvertiserEntity{}, result.Error
+ }
+ return entity, nil
 }
