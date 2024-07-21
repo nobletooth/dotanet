ec82055495b58be6178c8c7250c08c0010de7e26
Amirata Ghaffarian <amirata.gh.m.n@gmail.com>
2024-07-21 15:33:32 +0330

    template for 2 api.

--- publisherwebsite/main.go
+++ publisherwebsite/main.go
@@ -1,15 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
+}
 
 func main() {
 	pgConnection()
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/**/*")
 
-	router.GET("/")
-	router.POST("/")
+	// varzesh3Handler
+	router.GET("/varzesh3", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "varzesh3/index.tmpl", gin.H{
+			"title": "Varzesh3",
+		})
+	})
+
+	// digikalaHandler
+	router.GET("/digikala", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "digikala/index.tmpl", gin.H{
+			"title": "Digikala",
+		})
+	})
+
+	router.GET("/", defaultHandler)
+	router.POST("/", defaultHandler)
 
 	router.Run(":6060")
+}
+
+//func varzesh3Handler(content string) gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		c.String(200, "Welcome to Varzesh3!")
+//	}
+//}
+//
+//func digikalaHandler(content string) gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		c.String(200, "Welcome to Digikala!")
+//	}
+//}
+
+// defaultHandler
+func defaultHandler(c *gin.Context) {
+	c.String(200, "Welcome to API server!")
+}
+
+func pgConnection() {
+
 }
