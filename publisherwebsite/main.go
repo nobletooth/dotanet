edb317490efd932427e746ffbb74f4411cafc669
Amin-mhr <amin.mhr2001@gmail.com>
2024-07-21 12:39:04 +0330

    initialise gin

--- publisherwebsite/main.go
+++ publisherwebsite/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	pgConnection()
+	router := gin.Default()
+
+	router.GET("/")
+	router.POST("/")
+
+	router.Run(":6060")
 }
