bc2109c5c49e51d76cc679d458915c187218b6ed
dena <38652032+dena99@users.noreply.github.com>
2024-07-22 18:47:06 +0330

    separating ad retriever from main

--- adserver/main.go
+++ adserver/main.go
@@ -2,59 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 
-	"github.com/gin-contrib/multitemplate"
+	"example.com/dotanet/common"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-type Ad struct {
-	Id           uint    `gorm:"column:id;primary_key"`
-	Title        string  `gorm:"column:title"`
-	Image        string  `gorm:"column:image"`
-	Price        float64 `gorm:"column:price"`
-	Status       bool    `gorm:"column:status"`
-	Clicks       int     `gorm:"column:clicks"`
-	Impressions  int     `gorm:"column:impressions"`
-	Url          string  `gorm:"column:url"`
-	AdvertiserId uint64  `gorm:"foreignKey:AdvertiserId"`
-}
-
-var allAds []Ad
+var allAds []common.Ad
 
 func main() {
 	router := gin.Default()
-	var db *gorm.DB
-	r := multitemplate.NewRenderer()
-	r.AddFromFiles("ads_list", "./templates/ads_list.html")
+	router.GET("/ads/", GetAdsHandler)
+	go GetAdsListPeriodically(allAds)
 	fmt.Println("Server running on port 8080")
-	go GetAdsListPeriodically(db, router)
 	router.Run(":8080")
-
 }
 
-func GetAdsListPeriodically(db *gorm.DB, router *gin.Engine) {
-	ticker := time.NewTicker(1 * time.Minute)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			router.GET("/ads/", func(c *gin.Context) {
-				c.JSON(http.StatusOK, allAds)
-				c.HTML(http.StatusOK, "ads_list.html", gin.H{
-					"title": "All Ads",
-					"ads":   allAds,
-				})
-			})
-
-			for _, ad := range allAds {
-				log.Printf("Ad ID: %d, Title: %s", ad.Id, ad.Title)
-			}
-			log.Println("Ads list fetched successfully")
-		}
-	}
+func GetAdsHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, allAds)
 }
-
