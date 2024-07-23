97e8598e6a86c6dfd3e53221ed3e16667afd969c
Amirata Ghaffarian <amirata.gh.m.n@gmail.com>
2024-07-23 11:26:17 +0330

    adserver to main

--- adserver/main.go
+++ adserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"example.com/dotanet/common"
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,13 @@ var allAds []common.AdInfo
 
 func main() {
 	router := gin.Default()
+	router.GET("/ads/", GetAdsHandler)
+
 	go GetAdsListPeriodically(allAds)
 	fmt.Println("Server running on port 8080")
 	router.Run(":8080")
+}
+
+func GetAdsHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, allAds)
 }
