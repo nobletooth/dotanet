9fbe8a5f075db7ee60c6e993f21ab3efc630394c
Amin-mhr <amin.mhr2001@gmail.com>
2024-07-23 15:37:38 +0330

    pulisher main.go

--- publisherwebsite/main.go
+++ publisherwebsite/main.go
@@ -1,46 +1,34 @@
 package main
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	//pgConnection()
 	router := gin.Default()
-	router.LoadHTMLGlob("./html/*")
+	router.LoadHTMLGlob("./publisherwebsite/html/*")
 
+	router.GET("/:sitename", siteHandler())
 
 	router.Run(":6060")
 }
 
-func torobHandler() gin.HandlerFunc {
+func siteHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "torob.html", gin.H{})
-	}
-}
-
-func samsungHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "samsung.html", gin.H{})
-	}
-}
-
-func digilandHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "digiland.html", gin.H{})
-	}
-}
-
-func varzesh3Handler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "varzesh3.html", gin.H{})
-	}
-}
-
-func digikalaHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "digikala.js.html", gin.H{})
+		sitename := c.Param("sitename")
+		var siteExist = false
+		for _, value := range sitenames {
+			if value == sitename {
+				siteExist = true
+			}
+		}
+		if !siteExist {
+			c.String(http.StatusBadRequest, "error : this site does not exist.")
+		}
+		htmladdress := fmt.Sprintf("%v.html", sitename)
+		c.HTML(http.StatusOK, htmladdress, gin.H{})
 	}
 }
