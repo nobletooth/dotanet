c1350187b263ac24b0c8fb9b99767938355acf52
Amirata Ghaffarian <amirata.gh.m.n@gmail.com>
2024-07-23 14:12:12 +0330

    Image path fetching in auction.go implemented

--- adserver/auction.go
+++ adserver/auction.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"sort"
 	"strconv"
 
-	_ "example.com/dotanet/common"
 	"github.com/gin-gonic/gin"
 )
+
+func GetImagePath(adID uint) (string, error) {
+	url := fmt.Sprintf("http://localhost:8080/ads/%d/pictures", adID)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get image path, status code: %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
 
 func GetAdHandler(c *gin.Context) {
 	pubID := c.Param("pubID")
 
 	if len(allAds) == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "No ads available"})
+		c.JSON(http.StatusOK, gin.H{
+			"Title":          "Default Ad",
+			"ImagePath":      "path/to/default/image.jpg",
+			"ClicksURL":      fmt.Sprintf("/click/%d/%d", 0, pubID),
+			"ImpressionsURL": fmt.Sprintf("/impression/%d/%d", 0, pubID),
+		})
 		return
 	}
 
@@ -31,9 +56,15 @@ func GetAdHandler(c *gin.Context) {
 		return
 	}
 
+	imagePath, err := GetImagePath(bestAd.Id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get image path"})
+		return
+	}
+
 	response := gin.H{
 		"Title":          bestAd.Title,
-		"ImagePath":      bestAd.Image,
+		"ImagePath":      imagePath,
 		"ClicksURL":      fmt.Sprintf("/click/%d/%d", bestAd.AdvertiserId, publisherID),
 		"ImpressionsURL": fmt.Sprintf("/impression/%d/%d", bestAd.AdvertiserId, publisherID),
 	}
