c71f0f82b011575190a39e844a918fcb7c829f54
dena <38652032+dena99@users.noreply.github.com>
2024-07-24 18:38:30 +0330

    flag

--- panel/database/db.go
+++ panel/database/db.go
@@ -16,7 +16,7 @@ var (
 	dbname    = flag.String("dbname", "3", "Database name")
 	port      = flag.String("dbport", "4", "Database port")
 	host      = flag.String("dbhost", "5", "Database host")
-	PanelPort = flag.String("panelport", ":8081", "Panel port")
+	PanelUrl = flag.String("panelurl", "localhost:8081", "Panel url")
 )
 
 func NewDatabase() error {
