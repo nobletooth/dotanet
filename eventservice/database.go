333544ee177fcab133449de0fd736e92e2c805e5
sobhanala <91007417+sobhanala@users.noreply.github.com>
2024-07-24 14:06:01 +0330

    flag format eventserver

--- eventservice/database.go
+++ eventservice/database.go
@@ -16,12 +16,12 @@ var (
 	dbname           = flag.String("dbname", "todo", "Database name")
 	port             = flag.String("dbport", "5432", "Database port")
 	host             = flag.String("dbhost", "localhost", "Database host")
-	EventservicePort = flag.String("panelport", ":8082", "Panel port")
+	EventservicePort = flag.String("panelport", "8082", "Panel port")
 )
 
 func OpenDbConnection() (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		*host, *user, *password, *dbname, *port)
+		host, user, password, dbname, port)
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
