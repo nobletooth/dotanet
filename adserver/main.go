1cdfe91302e86c3227e23092a9255aeaac0f5cbb
Amin-mhr <amin.mhr2001@gmail.com>
2024-07-27 17:50:48 +0330

    changes flag name.

--- adserver/main.go
+++ adserver/main.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	allAds                            []common.AdWithMetrics
-	AdserverPort                      = flag.String("adserverport", ":8081", "ad server port")
+	AdserverPort                      = flag.String("adserverurl", ":8081", "ad server port")
 	PanelUrl                          = flag.String("panelurl", "http://localhost:8080", "panel url")
 	NewAdImpressionThreshold          = flag.Int64("newAdTreshold", 5, "Impression threshold for considering an ad as new")
 	NewAdSelectionProbability         = flag.Float64("newAdProb", 0.25, "Probability of selecting a new ad")
