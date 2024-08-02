9f9120ca8205773edb4c85961e5dc4e568cc10ee
Amirata Ghaffarian <amirata.gh.m.n@gmail.com>
2024-08-02 20:13:24 +0330

    setup router

--- adserver/auction_test.go
+++ adserver/auction_test.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 // Mock Random Generator
 type mockRandomGenerator struct {
 	float64Result float64
@@ -12,4 +16,10 @@ func (m *mockRandomGenerator) Float64() float64 {
 
 func (m *mockRandomGenerator) Intn(n int) int {
 	return m.intnResult
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ad/:pubID", GetAdHandler)
+	return r
 }
