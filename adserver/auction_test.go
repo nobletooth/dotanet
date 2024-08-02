69c7ca9a0a5be8a3b0c5a3455f190c7930362fac
Amirata Ghaffarian <amirata.gh.m.n@gmail.com>
2024-08-02 20:23:06 +0330

    sub-test No ads available

--- adserver/auction_test.go
+++ adserver/auction_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"common"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"testing"
 )
 
 // Mock Random Generator
@@ -22,4 +25,22 @@ func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ad/:pubID", GetAdHandler)
 	return r
+}
+
+func TestGetAdHandler(t *testing.T) {
+	tests := []struct {
+		name          string
+		allAds        []common.AdWithMetrics
+		randFloat     float64
+		randInt       int
+		expectedCode  int
+		expectedTitle string
+	}{
+		{
+			name:          "No ads available",
+			allAds:        []common.AdWithMetrics{},
+			expectedCode:  http.StatusNotFound,
+			expectedTitle: "",
+		},
+	}
 }
